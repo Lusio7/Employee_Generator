7dcbf4f4065d0c6155e5474e9956605d68c73ec8
LusioF <lusiof@gmail.com>
2020-01-29 20:56:34 -0500

    building index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const inquirer = require("inquirer");
+
+const render = require("./lib/HTMLrender");
+
+const Manager = require("./lib/constructors/Manager");
+const Engineer = require("./lib/constructors/Engineer");
+
+const teamMembers = [
+    //   new Manager("Manny", 1, "manny@heiscool.com", 200),
+    //   new Engineer("Chaz", 2, "chaz@heiscool.com", "viachaz"),
+    //   new Intern("vas", 3, "vas@heiscool.com", "UofA"),
+    //   new Intern("Caleb", 4, "caleb@heiscool.com", "ASU")
+    ];
+
+    
